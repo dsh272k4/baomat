959eecc3d94caaebf490e00615da20ec4b2fa540
dsh272k4 <dsh272004@gmail.com>
2025-11-14 00:38:23 +0700

    lần camn

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -7,8 +7,8 @@ dotenv.config();
 export const createTransporter = () => {
     return nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
-        port: Number(process.env.EMAIL_PORT) || 587,
-        secure: false, // Hostinger chỉ hỗ trợ STARTTLS
+        port: Number(process.env.EMAIL_PORT) || 465,
+        secure: true, // Hostinger chỉ hỗ trợ STARTTLS
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
